bb408a5a2a558a72c8a667217445e614d81dac3d
athenasj <athenasj_23@yahoo.com>
2018-04-01 11:06:25 +0800

    Added background audio.

---
+++ Assets/The Scripts/LandingScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LandingScreen : MonoBehaviour {
+
+    public GameObject BGAudio;
+    void Awake()
+    {
+        DontDestroyOnLoad(BGAudio);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void GoToGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
